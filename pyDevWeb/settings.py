4a1665439dc70787a4947971d7a8f11b6e79c4e3
wangchenming <charblus7582@gmail.com>
2019-06-24 10:25:53 +0800

    setting

--- pyDevWeb/settings.py
+++ pyDevWeb/settings.py
@@ -77,9 +77,18 @@ WSGI_APPLICATION = 'pyDevWeb.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meizi_db',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'root',
+        'PORT': 3306,
+        'OPTIONS': {'charset', 'utfmb4'},
     }
 }
 
